8e932b07a2873875e9f69da1c379c7ee02932b6b
Alexa Meza <indiramezz@gmail.com>
2024-10-08 21:29:51 +0000

    initial commit

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x885Ed703cc47dF7BBdD34eFF4dA0d64D577ff897'
+export const ProxyAdmin = '0xeEDA8Ad81b382eF32e244fEBDe23952a45a1eCd1'
+export const SWTRImplementation = '0xdc908975Ac400aF64Ecc0699DC92ff6e8e192300'
+    
